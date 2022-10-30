87de18b2cfa7f55ceb4a811b3ea49c0f2d366ac9
CrayzyCray <85997602+CrayzyCray@users.noreply.github.com>
2022-10-30 14:50:31 +0500

    Commit to save

---
+++ StreamerWinui/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace StreamerWinui
+{
+    public sealed partial class MainWindow : Window
+    {
+        const double defaultFramerate = 30;
+        const string defaultIpToStream = "localhost";
+
+        StreamSession streamSession;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            //выключение корировщика при закрытии
+            this.Closed += (s, e) => { if (streamSession.StreamIsActive) { streamSession.stopStream(); } };
+
+            streamSession = new StreamSession();
+
+            //заполнение codecComboBox
+            List<string> userFriendlyCodecNames = new List<string>();
+            foreach (var item in streamSession.supportedCodecs)
+            {
+                userFriendlyCodecNames.Add(item.userFriendlyName);
+            }
+            codecComboBox.ItemsSource = userFriendlyCodecNames;
+            codecComboBox.SelectedIndex = 0;
+
+            //кастомный тайтлбар
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(appTitleBar);
+
+            //установка размера окна
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 240, Height = 300 });
+        }
+
+        private void startStreamButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!streamSession.StreamIsActive)
+            {
+                double framerate = defaultFramerate;
+                string ipToStream = defaultIpToStream;
+
+                if (framerateTextBlock.Text != "")
+                    try
+                    {
+                        framerate = Convert.ToDouble(framerateTextBlock.Text);
+                    }
+                    catch { }
+
+                if (ipTextBlock.Text != "")
+                    ipToStream = ipTextBlock.Text;
+
+                string codec = streamSession.supportedCodecs[codecComboBox.SelectedIndex].name;
+
+                streamSession.startStream(codec, framerate, ipToStream, showConsoleCheckBox.IsChecked.GetValueOrDefault());
+                startStreamButton.Content = "Stop";
+                showConsoleCheckBox.IsEnabled = false;
+                framerateTextBlock.IsEnabled = false;
+                ipTextBlock.IsEnabled = false;
+            }
+            else
+            {
+                streamSession.stopStream();
+                startStreamButton.Content = "Start";
+                showConsoleCheckBox.IsEnabled = true;
+                framerateTextBlock.IsEnabled = true;
+                ipTextBlock.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ StreamerWinui/StreamSession.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamerWinui
+{
+    public class StreamSession
+    {
+        public bool StreamIsActive { get { return streamIsActive; } }
+        public Process FfmpegProcess { get { return ffmpegProcess; } }
+
+        bool streamIsActive = false;
+        Process ffmpegProcess = new Process();
+
+        public struct Codec
+        {
+            public Codec(string _userFriendlyName, string _name)
+            {
+                userFriendlyName = _userFriendlyName;
+                name = _name;
+            }
+            public string userFriendlyName { get; }
+            public string name { get; }
+        }
+        public Codec[] supportedCodecs =
+        {
+            new Codec("hevc Nvidia", "hevc_nvenc"),
+            new Codec("hevc AMD", "hevc_amf"),
+            new Codec("hevc Intel", "hevc_qsv"),
+            new Codec("h264 Nvidia", "h264_nvenc"),
+            new Codec("h264 AMD", "h264_amf"),
+            new Codec("h264 Intel", "h264_qsv")
+        };
+
+
+        public void startStream(string codec, double framerate, string ipToStream, bool showConsole)
+        {
+            //путь к ffmpeg
+            string pathToFfmpegFile = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            pathToFfmpegFile = Path.Combine(pathToFfmpegFile, "ffmpeg.exe");
+
+            string arguments = $"ffmpeg.exe -init_hw_device d3d11va -filter_complex ddagrab=0:framerate={framerate} -c:v {codec} -cq:v 20 -bf 0 -f mpegts rist://{ipToStream}:10000";
+
+            ffmpegProcess.StartInfo.FileName = "cmd.exe";
+            ffmpegProcess.StartInfo.Arguments = $"/c {pathToFfmpegFile} & {arguments}" +" & pause";
+            ffmpegProcess.StartInfo.CreateNoWindow = !showConsole;
+
+            ffmpegProcess.Start();
+            streamIsActive = true;
+            
+        }
+
+        public void stopStream()
+        {
+            ffmpegProcess.Kill(true);
+            streamIsActive = false;
+        }
+    }
+}
